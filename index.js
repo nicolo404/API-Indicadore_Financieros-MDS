8983fc1a842a2692c89d6f844f456f9f989fac6f
Nicolas Silva (Talcahuano) <nsilva@marinadelsol.cl>
2024-01-26 09:51:47 -0300

    primer commit

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+const myLogger = function (req, res, next) {
+    console.log('LOGGED')
+    next()
+  }
+  
+  app.use(myLogger)
+  
+  app.get('/', (req, res) => {
+    res.send('Hello World!')
+  })
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`);
+});
+
+
+
